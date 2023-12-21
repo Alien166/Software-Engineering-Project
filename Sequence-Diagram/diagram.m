b7abcd6b70f3c2297f461e256de3cadca5e45ed7
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-12-21 20:56:28 +0200

    Create diagram.m

---
+++ Sequence-Diagram/diagram.m
@@ -0,0 +1,30 @@
+## Activity Diagram in Clinic Management System (CMS)
+
+### Components:
+1. **Actions/Nodes:** Represent tasks or actions performed within the system.
+2. **Transitions/Edges:** Arrows indicating the flow or sequence of actions.
+3. **Decision Points:** Points where the flow of activities diverges based on certain conditions.
+4. **Initial/Final Nodes:** Represent the start and end points of the activity.
+
+### Purpose:
+Activity diagrams outline the sequence of activities performed by actors to achieve a specific goal or use case in the CMS. For instance:
+- Patient registration process.
+- Appointment scheduling workflow.
+- Managing patient records.
+
+### Benefits:
+- **Clarity:** Offers a clear visual representation of the steps involved in a particular process or workflow.
+- **Process Optimization:** Helps in identifying redundant steps and streamlining processes within the CMS.
+- **Communication:** Acts as a communication tool among stakeholders to understand system workflows and functionalities.
+
+### 1.patient
+![Alt text](image_url "patient")
+
+
+### 2.Doctor
+![Alt text](image_url "Doctor")
+
+
+### 3.Admin
+![Alt text](image_url "Admin ")
+
